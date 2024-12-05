45a3b5eac13a001adf9b976d4ddf30eb9e93ea57
M373746 <j.beskow@sistran.com.br>
2024-12-05 17:01:14 -0300

    criacao e edicao do componente chamado

---
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Chamado } from 'src/app/models/chamado';
+
+@Component({
+  selector: 'app-chamado-list',
+  standalone: false,
+  templateUrl: './chamado-list.component.html',
+  styleUrl: './chamado-list.component.css'
+})
+export class ChamadoListComponent implements OnInit{
+
+  ELEMENT_DATA: Chamado[] = []
+
+  displayedColumns: string[] = ['id', 'titulo', 'cliente','tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  ngOnInit(): void {
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
