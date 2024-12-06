18c5d66eba0e399a217469c65830f15c37cf8015
M373746 <j.beskow@sistran.com.br>
2024-12-06 10:45:08 -0300

    validacao dos components criacao-chamado

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chamado-create',
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './chamado-create.component.html',
   styleUrl: './chamado-create.component.css'
 })
-export class ChamadoCreateComponent {
+export class ChamadoCreateComponent implements OnInit{
+
+  prioridade: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  titulo: FormControl = new FormControl(null, [Validators.required])
+  descricao: FormControl = new FormControl(null, [Validators.required])
+  tecnico: FormControl = new FormControl(null, [Validators.required])
+  cliente: FormControl = new FormControl(null, [Validators.required])
+
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid &&
+           this.status.valid &&
+           this.titulo.valid &&
+           this.descricao.valid &&
+           this.tecnico.valid &&
+           this.cliente.valid
+  }
 
 }
